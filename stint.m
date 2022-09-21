b3fe44b399c81014375d7c7d654287d698b552d3
Dani Agramonte <daniel.agramonte@uga.edu>
2022-09-21 12:42:12 -0400

    code analyzer message suppression
    didn't grab one line for previous commit

--- stint.m
+++ stint.m
@@ -9,6 +9,10 @@ dW = sqrt(dt)*randn(1,N);               % increments
 W = cumsum(dW);                         % cumulative sum
 
 ito = sum([0,W(1:end-1)].*dW)    
+% suppress code analyzer message
+%#ok<*NOPTS>
+
+ito = sum([0,W(1:end-1)].*dW)
 strat = sum((0.5*([0,W(1:end-1)]+W) + 0.5*sqrt(dt)*randn(1,N)).*dW) 
 
 itoerr = abs(ito - 0.5*(W(end)^2-T))
